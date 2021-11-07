6ae4132a5746cb43d8bf027d486a1a6023a02d30
myusufh26 <1910817210003@mhs.ulm.ac.id>
2021-11-07 17:57:34 +0800

    update

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta name="author" content="SemiColonWeb" />
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Login</title>
     <style>
         .hed{
             background: #ccc;
-            color: blue;
+            color: black;
         }
         .hed h1{
             padding-top: 20px;
@@ -30,29 +30,25 @@
     <div id="wrapper" class="clearfix">
         <section id="page-title">
             <div class="container clearfix hed">
-                <center>
-                </center>
             </div>
         </section>
         <div class="col-md-12 hed">
-            <center>
                 <h1>Insert Data</h1>
-            </center>
         </div>
         <section id="content">
             <div class="content-wrap">
                 <div class="container clearfix">
                     <div class="row">
-                        <div class="col-md-12" id="hide">
+                        <div class="col-md-5" id="hide">
                             <form  class="row form" action="insert.php" method="post">
                                 <?php include 'form.php'; ?>
                                 <div class="col-12 form-group">
-                                    <input type="submit" class="btn btn-dark" name="submit" value="Insert">
+                                    <input type="submit" class="btn btn-secondary float-right p-2 mb-1 bg-success text-white" name="submit" value="Insert">
                                 </div>
                             </form>
                         </div>
                         <div class="col-md-12 p-0">
-                            <table class="table table-dark">
+                            <table class="table table-warning text-dark">
                                 <thead>
                                     <tr>
                                         <th scope="col">Id</th>
