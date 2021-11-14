e534c8fa60fd31f5a231c0d4bee7a716b0b2bede
myusufh26 <1910817210003@mhs.ulm.ac.id>
2021-11-14 08:10:59 +0800

    update sql

--- oop_test.sql
+++ oop_test.sql
@@ -31,7 +31,7 @@ CREATE TABLE `data` (
   `id` int(11) NOT NULL,
   `name` varchar(55) NOT NULL,
   `email` varchar(55) NOT NULL,
-  `phone` int(55) NOT NULL,
+  `phone` varchar(250) NOT NULL,
   `subject` text NOT NULL,
   `message` varchar(55) NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -41,14 +41,6 @@ CREATE TABLE `data` (
 --
 -- Dumping data for table `data`
 --
-
-INSERT INTO `data` (`id`, `name`, `email`, `phone`, `subject`, `message`, `created`, `updated`) VALUES
-(1, 'qwe', '12313@gmeail.com', 0, 'anjey', '321313', '2021-11-07 05:30:16', '2021-11-07 06:30:16'),
-(2, 'arya', 'eqweqe@gmail.com', 123232, '123ddaaada', 'anjay\r\n3123', '2021-11-06 21:45:17', '0000-00-00 00:00:00'),
-(3, '123', '12133@gmail', 123, '13', '123', '2021-11-06 22:09:27', '0000-00-00 00:00:00'),
-(6, 'arya', '123@gmail.com', 211212, 'der', 'red', '2021-11-06 22:53:14', '0000-00-00 00:00:00'),
-(9, 'asdr', '1910817210004@mhs.ulm.ac.id', 312311, 'uytre', 'qwerty', '2021-11-06 23:19:21', '0000-00-00 00:00:00'),
-(10, 'asdr', '1910817210004@mhs.ulm.ac.id', 312311, 'uytre', '', '2021-11-06 23:20:45', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -68,7 +60,7 @@ ALTER TABLE `data`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
